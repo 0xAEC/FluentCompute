9c5c0995def06e44762d43616f135a9c4c1d16ce
Pulsar AI <omar.hussein@legacyiohs.org>
2025-06-16 16:11:56 -0400

    Update __init__.py

--- fluentcompute/detectors/__init__.py
+++ fluentcompute/detectors/__init__.py
@@ -1,1 +1,17 @@
+from .base import HardwareDetector
+from .nvidia_detector import NvidiaDetector
+from .amd_detector import AMDDetector
+from .intel_detector import IntelDetector
+from .apple_detector import AppleDetector
+from .cloud_detector import CloudDetector
+from .unknown_detector import UnknownDetector
 
+__all__ = [
+    "HardwareDetector",
+    "NvidiaDetector",
+    "AMDDetector",
+    "IntelDetector",
+    "AppleDetector",
+    "CloudDetector",
+    "UnknownDetector"
+]
