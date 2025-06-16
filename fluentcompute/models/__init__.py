76368f42d52aa158f92cdf1f6c5f6233b9c736c6
Pulsar AI <omar.hussein@legacyiohs.org>
2025-06-16 17:56:55 -0400

    Update __init__.py

--- fluentcompute/models/__init__.py
+++ fluentcompute/models/__init__.py
@@ -1,7 +1,16 @@
+# fluentcompute/models/__init__.py
 from .enums import VendorType, ComputeCapability, PerformanceTier, OptimizationStrategy
-from .data_schemas import TelemetryData, BenchmarkResult, GPUInfo
+from .data_schemas import (
+    TelemetryData, 
+    BenchmarkResult, 
+    GPUInfo,
+    FrameworkDependency, # New
+    FrameworkInfo,       # New
+    PythonEnvironmentInfo # New
+)
 
 __all__ = [
     "VendorType", "ComputeCapability", "PerformanceTier", "OptimizationStrategy",
-    "TelemetryData", "BenchmarkResult", "GPUInfo"
+    "TelemetryData", "BenchmarkResult", "GPUInfo",
+    "FrameworkDependency", "FrameworkInfo", "PythonEnvironmentInfo" # Export new models
 ]
