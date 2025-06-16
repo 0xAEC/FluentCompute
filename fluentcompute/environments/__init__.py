8a0f796d8f2614b1977568dd0bee4fa3f562ca1d
Pulsar AI <omar.hussein@legacyiohs.org>
2025-06-16 18:38:49 -0400

    Update __init__.py

--- fluentcompute/environments/__init__.py
+++ fluentcompute/environments/__init__.py
@@ -1,3 +1,5 @@
+# fluentcompute/environments/__init__.py
 from .detector import EnvironmentDetector
+from .manager import EnvironmentManager # New export
 
-__all__ = ["EnvironmentDetector"]
+__all__ = ["EnvironmentDetector", "EnvironmentManager"]
